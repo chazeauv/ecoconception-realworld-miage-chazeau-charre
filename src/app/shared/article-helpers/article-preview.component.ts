4a4f261ed3180a02bbf548c4b7fe720a61f4c1d4
l3miage-chazeauv <vincent.chazeau05@gmail.com>
2025-01-06 11:47:58 +0100

    BP 59 - Éviter les temps de blocages par des traitements JavaScript trop longs

--- src/app/shared/article-helpers/article-preview.component.ts
+++ src/app/shared/article-helpers/article-preview.component.ts
@@ -3,16 +3,43 @@ import { Article } from "../../core/models/article.model";
 import { ArticleMetaComponent } from "./article-meta.component";
 import { FavoriteButtonComponent } from "../buttons/favorite-button.component";
 import { RouterLink } from "@angular/router";
-import { NgForOf } from "@angular/common";
+import { NgForOf, NgIf } from "@angular/common";
 
 @Component({
   selector: "app-article-preview",
   templateUrl: "./article-preview.component.html",
-  imports: [ArticleMetaComponent, FavoriteButtonComponent, RouterLink, NgForOf],
+  imports: [
+    ArticleMetaComponent,
+    FavoriteButtonComponent,
+    RouterLink,
+    NgForOf,
+    NgIf,
+  ],
   standalone: true,
 })
 export class ArticlePreviewComponent {
   @Input() article!: Article;
+  okArticle: boolean = false;
+
+  constructor() {
+    this.changerOkArticle();
+  }
+
+  changerOkArticle() {
+    console.log("okArticle start");
+    this.heavyCalculation();
+    console.log("okArticle end");
+    this.okArticle = true;
+  }
+
+  heavyCalculation() {
+    let sum = 0;
+    for (let i = 0; sum < 400000; i++) {
+      sum += i;
+      console.log(sum);
+    }
+    return sum;
+  }
 
   toggleFavorite(favorited: boolean): void {
     this.article.favorited = favorited;
